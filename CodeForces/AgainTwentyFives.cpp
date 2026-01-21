47169b4b9beb0561d9dcca2c9f3ab4c5253d1014
Chayut Auanoppakun <107758955+Chayut-Auanoppakun@users.noreply.github.com>
2026-01-21 15:12:47 +0700

    Delete CodeForces directory

--- CodeForces/AgainTwentyFives.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    if(n==1) cout<<"5";
-    else cout<<"25";
-}
--- CodeForces/VusTheCossack.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n,p,c;
-    cin>>n>>p>>c;
-    if(n>p || n>c) cout<<"No";
-    else cout<<"Yes";
-}
